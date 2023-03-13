a54aa2b13e481067bc39646af41cd35572c7b30e
shruti.arora <shruti.arora@modmed.com>
2023-03-14 02:34:02 +0530

    add one more sop

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -2,6 +2,7 @@ public class FirstTest {
 
     public static void main(String[] args){
         System.out.println("My first test to learn Jenkins");
+        System.out.println("Add one more print statement to check auto trigger");
 
     }
 }
